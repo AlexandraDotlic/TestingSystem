dbd61212d9f68fdad19b50a8f48c86d60b488f36
bozzano101 <boskonet@gmail.com>
2020-12-22 23:30:09 +0100

    Added Test class that describes test entity from schema

---
+++ TestingSystem/Core/Domain/Entites/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Domain.Entites.Questions;
+
+namespace Core.Domain.Entites
+{
+    class Test
+    {
+        public short Id { get; private set; }
+        public string Title { get; private set; }
+        public ICollection<BaseQuestion> Questions { get; private set; }
+
+        // GroupId to be added
+
+        public Test(short _Id, string _Title, ICollection<BaseQuestion> _Questions) 
+        {
+            Id = _Id;
+            Title = _Title;
+            Questions = _Questions;
+        }
+
+        public Test()
+        {
+
+        }
+    }
+}
