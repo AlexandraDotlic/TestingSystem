30fac761f973d61190060c905049108f7ab6084d
Andrijana Pekovic <pekovic.andrijana@gmail.com>
2020-12-22 19:18:36 +0100

    Core.Domain.Entities.Questions: created MultipleChoiceQuestion

---
+++ TestingSystem/Core/Domain/Entites/Questions/MultipleChoiceQuestion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Entites.Questions
+{
+    class MultipleChoiceQuestion : BaseQuestion
+    {
+        public bool Answer { get; private set; }
+
+        public MultipleChoiceQuestion() : base()
+        { }
+
+        public MultipleChoiceQuestion(string question, bool answer) : base(question)
+        {
+            Answer = answer;
+        }
+    }
+}
