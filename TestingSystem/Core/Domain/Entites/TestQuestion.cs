16b2a2db63b3cd74e6307626e2248238c6fbe3ef
Andrijana Pekovic <pekovic.andrijana@gmail.com>
2020-12-23 16:10:19 +0100

    Core.Domain.Entities: created TestQuestion

---
+++ TestingSystem/Core/Domain/Entites/TestQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Entites
+{
+    class TestQuestion
+    {
+        public int TestId { get; private set; }
+        public int QuestionId { get; private set; }
+
+        public TestQuestion() { }
+
+    }
+}
