2cff6466b70401546735f24829b94e8ceb5435c6
Mirjana <mirjanastefy@gmail.com>
2020-12-22 20:53:44 -0800

    Core.Domain.Entities.Student: created Student

---
+++ TestingSystem/Core/Domain/Entites/Student/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Entites.Student
+{
+    class Student
+    {
+
+        public int Id { get; private set; }
+    
+        public string FirstName { get; protected set; }
+
+        public string LastName { get; protected set; }
+
+        // ICollection<Group> to be added
+
+        public Student()
+        { }
+
+        
+        public Student(int id, string firstname, string lastname)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+        }
+    }
+}
