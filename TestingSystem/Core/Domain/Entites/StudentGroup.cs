9c411fb7be7d1993be3826d2048f446eb98e907c
Andrijana Pekovic <pekovic.andrijana@gmail.com>
2020-12-23 16:09:29 +0100

    Core.Domain.Entities: created StudentGroup

---
+++ TestingSystem/Core/Domain/Entites/StudentGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Entites
+{
+    public class StudentGroup
+    {
+        public int GroupId { get; private set; }
+        public int StudentId { get; private set; }
+        public string StudentResponse { get; private set; }
+
+        public StudentGroup() { }
+
+        public StudentGroup(string studentResponse) : this()
+        {
+            StudentResponse = studentResponse;
+        }
+
+    }
+}
