486f7b8b12dc5dd0a21bdf5b8ee0cb7f59ad083c
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-01-16 17:13:22 +0100

    Created WebClientApplication and added folder DevEnvironment and docker-compose.yml

---
+++ TestingSystem/Applications/WebClient/Startup.cs
@@ -0,0 +1,56 @@
+using Core.Domain.Repositories;
+using Core.Infrastructure.DataAccess.EfCoreDataAccess;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<CoreEfCoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TestingSystemDevConnection"));
+            });
+
+            services.AddScoped<ICoreUnitOfWork, CoreEfCoreUnitOfWork>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
