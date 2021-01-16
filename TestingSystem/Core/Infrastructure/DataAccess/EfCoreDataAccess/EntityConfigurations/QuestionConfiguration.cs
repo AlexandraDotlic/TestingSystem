24bc24b054001ea51c2dcc734ef7dc7ad5336baf
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-01-16 16:48:41 +0100

    Core.Infrastructure.DataAccess: updated Test and Question configuration classes

---
+++ TestingSystem/Core/Infrastructure/DataAccess/EfCoreDataAccess/EntityConfigurations/QuestionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Core.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Infrastructure.DataAccess.EfCoreDataAccess.EntityConfigurations
+{
+    public class QuestionConfiguration : IEntityTypeConfiguration<Question>
+    {
+        public void Configure(EntityTypeBuilder<Question> builder)
+        {
+            builder.Property(tq => tq.Id).ValueGeneratedOnAdd();
+            builder.Property(tq => tq.QuestionText).HasMaxLength(1000);
+            builder.Property(tq => tq.Answer).HasMaxLength(5000);
+        }
+    }
+}
--- TestingSystem/Core/Infrastructure/DataAccess/EfCoreDataAccess/EntityConfigurations/TestConfiguration.cs
+++ TestingSystem/Core/Infrastructure/DataAccess/EfCoreDataAccess/EntityConfigurations/TestConfiguration.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Core.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Infrastructure.DataAccess.EfCoreDataAccess.EntityConfigurations
 {
-    class TestConfiguration
+    public class TestConfiguration : IEntityTypeConfiguration<Test>
     {
+        public void Configure(EntityTypeBuilder<Test> builder)
+        {
+            builder.Property(t => t.Id).ValueGeneratedOnAdd();
+            builder.Property(t => t.Title).HasMaxLength(50);
+        }
     }
 }
