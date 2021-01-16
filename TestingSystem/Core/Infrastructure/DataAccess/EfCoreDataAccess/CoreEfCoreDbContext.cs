800bb5086fe15df522c29fa0df007ce62456e335
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-01-16 15:49:59 +0100

    Core.Infrastructure.DataAccess.EfCoreDataAccess: implemented CoreEfCoreDbContext

---
+++ TestingSystem/Core/Infrastructure/DataAccess/EfCoreDataAccess/CoreEfCoreDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Common.EfCoreDataAccess;
+using Core.Domain.Entites;
+using Core.Domain.Entites.Questions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Infrastructure.DataAccess.EfCoreDataAccess
+{
+    public class CoreEfCoreDbContext : EfCoreDbContext
+    {
+
+        public CoreEfCoreDbContext(DbContextOptions<CoreEfCoreDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Examiner> Examiners { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Group> Groups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
