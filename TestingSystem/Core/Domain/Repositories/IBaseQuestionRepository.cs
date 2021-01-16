2eff0e1de86b0ce42b31f1d06d57fd85b2f736d1
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-01-16 15:34:53 +0100

    Core.Domain: Added repository interfaces for Test, Examiner, BaseQuestion, Group and Student.

---
+++ TestingSystem/Core/Domain/Repositories/IBaseQuestionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Common.Utils;
+using Core.Domain.Entites.Questions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Repositories
+{
+    public interface IBaseQuestionRepository: IRepository<BaseQuestion>
+    {
+    }
+}
