53a633dccc5c6f268b04c7eb23a66c1017fb2599
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-03-28 20:57:40 +0200

    Core.Infrastructure: Changed some entity configs and added new. Added migration for model change

---
+++ TestingSystem/Core/Infrastructure/DataAccess/EfCoreDataAccess/EntityConfigurations/AnsweOptionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Infrastructure.DataAccess.EfCoreDataAccess.EntityConfigurations
+{
+    public class AnsweOptionConfiguration : IEntityTypeConfiguration<AnswerOption>
+    {
+        public void Configure(EntityTypeBuilder<AnswerOption> builder)
+        {
+            builder.Property(ao => ao.OptionText).HasMaxLength(256);
+        }
+    }
+}
---
+++ TestingSystem/Core/Infrastructure/DataAccess/EfCoreDataAccess/EntityConfigurations/StudentTestQuestionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Core.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Infrastructure.DataAccess.EfCoreDataAccess.EntityConfigurations
+{
+    public class StudentTestQuestionConfiguration : IEntityTypeConfiguration<StudentTestQuestion>
+    {
+        public void Configure(EntityTypeBuilder<StudentTestQuestion> builder)
+        {
+
+          
+            
+        }
+    }
+}
