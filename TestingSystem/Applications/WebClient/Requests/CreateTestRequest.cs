a7e502d614f54f31c5eb9f85f3551a1d52f2a118
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-03-28 11:16:39 +0200

    Applications.WebClient.Controllers.TestController - implemented  CreateTest and AddQuestionToTest

---
+++ TestingSystem/Applications/WebClient/Requests/CreateTestRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Applications.WebClient.Requests
+{
+    [DataContract]
+    public class CreateTestRequest
+    {
+        [DataMember]
+        public string Title { get; set; }
+    }
+}
