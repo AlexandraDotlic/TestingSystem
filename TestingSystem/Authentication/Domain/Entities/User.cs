c668da6e5f592ff2f8654c483273e010cb2d9ede
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-03-29 19:11:31 +0200

    Created Authentication.Domain project and added User entity

---
+++ TestingSystem/Authentication/Domain/Entities/User.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authentication.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+
+        #region Properties
+
+        public int UserAccountId { get; private set; }
+
+
+        #endregion
+
+        #region Constructors
+
+        public User()
+        {
+        }
+
+        public User(string email)
+        {
+            UserName = email;
+            Email = email;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void SetUserAccountId(int userAccountId)
+        {
+            UserAccountId = userAccountId;
+        }
+
+        public void UpdateUserName(string userName)
+        {
+            Email = userName;
+            UserName = userName;
+        }
+
+        public void UpdateEmail(string email)
+        {
+            Email = email;
+            UserName = email;
+        }
+
+        #endregion Methods
+    }
+}
