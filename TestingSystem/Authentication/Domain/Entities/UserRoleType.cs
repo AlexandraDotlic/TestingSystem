f81174f9fc16634d49c7864da028ccabcccffb4b
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-03-31 17:00:09 +0200

    Configured Auth

---
+++ TestingSystem/Authentication/Domain/Entities/UserRoleType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authentication.Domain.Entities
+{
+    public enum UserRoleType: byte
+    {
+        Examiner = 1,
+        Student = 2
+    }
+}
