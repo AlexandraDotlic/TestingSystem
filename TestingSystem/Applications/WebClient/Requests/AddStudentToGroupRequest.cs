d06282d54e49de9309af93fc491d738f71c92a3d
Mirjana <mirjanastefy@gmail.com>
2021-04-04 17:10:08 -0700

    WebClient.Requests - Added CreateGroupRequest and AddStudentToGroupRequest

---
+++ TestingSystem/Applications/WebClient/Requests/AddStudentToGroupRequest.cs
@@ -0,0 +1,18 @@
+﻿using Applications.WebClient.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Applications.WebClient.Requests
+{
+    [DataContract]
+    public class AddStudentToGroupRequest
+    {
+        [DataMember]
+        public short GroupId { get; set; }
+        [DataMember]
+        public short StudentId { get; set; }
+    }
+}
