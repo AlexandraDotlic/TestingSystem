40eb943c30fef590a711842b10193e7ac068fc0e
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-04-12 14:42:03 +0200

    fixed error in AnswerOption to generate Id on add. Fixed problem in TestService.AddQuestionToText

---
+++ TestingSystem/Core/ApplicationServices/QuestionService.cs
@@ -0,0 +1,27 @@
+﻿using Core.ApplicationServices.DTOs;
+using Core.Domain.Entites;
+using Core.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ApplicationServices
+{
+    public class QuestionService
+    {
+        private readonly ICoreUnitOfWork UnitOfWork;
+        public QuestionService(ICoreUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public async Task<ICollection<QuestionDTO>> GetAllQuestionsForTest(short testId)
+        {
+            IReadOnlyCollection<Question> questions = await UnitOfWork.QuestionRepository.GetAllWithIncludesAsList(q => q.TestId == testId, q => q.AnswerOptions);
+            List<QuestionDTO> questionDTOs = questions.Select(q => new QuestionDTO(q.Id, q.QuestionText, q.TestId, q.QuestionScore, q.AnswerOptions)).ToList();
+            return questionDTOs;
+        }
+    }
+}
