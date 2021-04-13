332ea6c5c77e4d1d941dfa1a35c9ec690450b84a
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-04-13 13:33:25 +0200

    Core.Domain.Services.External - created external service IMailService

---
+++ TestingSystem/Core/Domain/Services/External/MailService/MailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Services.External.MailService
+{
+    public class MailRequest
+    {
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
