4a1e12cfa2fb7ee23a9c1ec21c1f2bd8c3b531ec
Mirjana <mirjanastefy@gmail.com>
2021-04-19 17:49:49 +0200

    Applications.WebClient.DTOs.GroupDTO and Core.ApplicationServices.DTOs.GroupDTO created

---
+++ TestingSystem/Core/ApplicationServices/DTOs/GroupDTO.cs
@@ -0,0 +1,28 @@
+﻿using Core.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.ApplicationServices.DTOs
+{
+    public class GroupDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ExaminerId { get; set; }
+        
+        public GroupDTO()
+        {
+
+        }
+
+        public GroupDTO(int id, string title, int examinerId)
+        {
+            Id = id;
+            Title = title;
+            ExaminerId = examinerId;
+        }
+        
+    }
+}
