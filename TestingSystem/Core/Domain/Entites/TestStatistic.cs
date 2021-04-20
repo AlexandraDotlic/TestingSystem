f99259bd1681c8d43efced573c6f968ea6efae4a
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-04-20 11:38:03 +0200

    Core.Domain.Entities - added TestStatistic entity

---
+++ TestingSystem/Core/Domain/Entites/TestStatistic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Entites
+{
+    public class TestStatistic
+    {
+        public long Id { get; private set; }
+        public int TestId { get; private set; }
+        public int ExaminerId { get; private set; }
+        public int PercentageOfStudentsWhoPassedTheTest { get; private set; }
+        public int NumberOfStudentsWhoTookTheTest { get; private set; }
+        public DateTime? FromDate { get; private set; }
+        public DateTime? ToDate { get; private set; }
+        public TestStatistic()
+        {
+                
+        }
+
+        public TestStatistic(
+            int testId, 
+            int examinerId, 
+            int percentageOfStudentsWhoPassedTheTest, 
+            int numberOfStudentsWhoTookTheTest,
+            DateTime? fromDate = null, 
+            DateTime? toDate = null
+            )
+        {
+            TestId = testId;
+            ExaminerId = examinerId;
+            PercentageOfStudentsWhoPassedTheTest = percentageOfStudentsWhoPassedTheTest;
+            NumberOfStudentsWhoTookTheTest = numberOfStudentsWhoTookTheTest;
+            FromDate = fromDate;
+            ToDate = toDate;
+        }
+    }
+}
