f4411385b726c6a691c501db47256f0510cbbe7d
Mirjana <mirjanastefy@gmail.com>
2021-04-27 17:06:32 +0200

    Tests.CoreAppplicationServicesTests - SetExaminerFirstNameTests and SetExaminerLastNameTests

---
+++ TestingSystem/Tests/CoreApplicationServicesTests/SetExaminerLastNameTests.cs
@@ -0,0 +1,54 @@
+﻿using Core.ApplicationServices;
+using Core.Domain.Repositories;
+using Core.Domain.Entites;
+using Core.Infrastructure.DataAccess.EfCoreDataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.CoreApplicationServicesTests
+{
+
+    [TestClass]
+    public class SetExaminerLastNameTests
+    {
+        private ICoreUnitOfWork CoreUnitOfWork;
+        private CoreEfCoreDbContext DbContext;
+        private ExaminerService ExaminerService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var dbContextFactory = new SampleDbContextFactory();
+            DbContext = dbContextFactory.CreateDbContext(new string[] { });
+            CoreUnitOfWork = new CoreEfCoreUnitOfWork(DbContext);
+
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            await DbContext.DisposeAsync();
+            DbContext = null;
+        }
+
+        [TestMethod]
+        public async Task SetExaminerLastNameSuccess()
+        {
+            int examinerId = await ExaminerService.CreateExaminer("Ime", "Prezime", "123");
+            var examiner = await CoreUnitOfWork.ExaminerRepository.GetById(examinerId);
+            await ExaminerService.SetExaminerLastName(examinerId, "Markovic");
+
+            Assert.AreEqual(examiner.LastName, "Markovic");
+        }
+
+        [TestMethod]
+        public async Task SetExaminerLastNameFail()
+        {
+            var examiner = await CoreUnitOfWork.ExaminerRepository.GetById(100);
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await ExaminerService.SetExaminerLastName(1000, "Markovic"), $"Examiner with with Id={100} doesn't existt");
+        }
+    }
+}
