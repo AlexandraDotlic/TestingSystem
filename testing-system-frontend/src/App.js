07a51d595c4acd10eeb577e94e59ebb17ae8025b
bozzano101 <boskonet@gmail.com>
2021-05-05 13:07:36 +0200

    Students successfuly getted from database instead hardcoded entries

--- testing-system-frontend/src/App.js
+++ testing-system-frontend/src/App.js
@@ -1,12 +1,13 @@
 import Header from './template/header.js'
-
+import StudentList from './components/studentlist/StudentList'
 import Home from './components/home/Home.js'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <StudentList></StudentList>
     </div>
   );
 }
--- testing-system-frontend/src/components/studentlist/StudentList.js
+++ testing-system-frontend/src/components/studentlist/StudentList.js
@@ -5,17 +5,28 @@ import Student from './Student'
 class StudentList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            students : []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("https://localhost:44329/Student/GetAllStudents").then( response => {
+            this.setState({students: response.data.students})
+        });
     }
 
     render() {
+        let students = this.state.students.map(student => {
+            return <Student key={student.id} id={student.id} firstName={student.firstName} lastName={student.lastName}></Student>
+        })
+
         return (
             <div className="w-50 mx-auto pt-3">
                     <h5> Select students from list to add to group: </h5>
                     <hr></hr>
                     <ul>
-                        <Student id={1} firstName={"Marko"} lastName={"Markovic"}></Student>
-                        <Student id={2} firstName={"Petar"} lastName={"Petrovic"}></Student>
-                        <Student id={3} firstName={"Nikola"} lastName={"Nikolic"}></Student>
+                        {students}
                     </ul>
                     <hr></hr>
                     <div className="text-center">
