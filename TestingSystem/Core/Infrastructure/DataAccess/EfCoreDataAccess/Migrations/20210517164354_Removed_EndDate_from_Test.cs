60662e8a928693a1fe788d2c005e28ad6420b18d
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-05-17 18:44:53 +0200

    Core.Infrastructure.DataAccess - added migration for removing end date from test

---
+++ TestingSystem/Core/Infrastructure/DataAccess/EfCoreDataAccess/Migrations/20210517164354_Removed_EndDate_from_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Infrastructure.DataAccess.EfCoreDataAccess.Migrations
+{
+    public partial class Removed_EndDate_from_Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Tests");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Tests",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
