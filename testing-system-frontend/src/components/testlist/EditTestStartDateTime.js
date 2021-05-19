29410a3dd94329e8b377378789ea67dad7d6f795
apekovic <pekovic.andrijana@gmail.com>
2021-05-19 21:57:53 +0200

    Created frontend class for editing stard date and time for test

---
+++ testing-system-frontend/src/components/testlist/EditTestStartDateTime.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import React from 'react';
+import 'bootstrap'
+import { min } from 'moment';
+
+class EditTestStartDateTime extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            testId: this.props.id,
+            testTitle: this.props.title,
+            startDate: "",
+            startTime: ""
+        };
+
+        this.startDateChange = this.startDateChange.bind(this);
+        this.startTimeChange = this.startTimeChange.bind(this);
+        this.submitButton = this.submitButton.bind(this)
+
+    }
+
+    startDateChange(event) {
+        this.setState({startDate: event.target.value});
+    }
+
+    startTimeChange(event) {
+        this.setState({startTime: event.target.value});
+    }
+
+    submitButton = (event) => {
+        let startDateParsed = Date.parse(this.state.startDate);
+
+        startDateParsed += parseInt(this.state.startTime.substring(0, 2)) * 3600000;
+        startDateParsed += parseInt(this.state.startTime.substring(3)) * 60000;
+
+        let startDateCombined = new Date(startDateParsed);
+
+        axios({
+            method: 'post',
+            url: 'https://localhost:44329/Test/ChangeStartDate',
+            data: {
+            testId: this.state.testId,
+            startDate: startDateCombined,
+            }
+        });
+        event.preventDefault();
+    }
+
+    render(){
+        return (
+        <div className = "container-fluid w-50 mx-auto pt-4"> 
+            <h3 className="text-center"> Change Start Date and Start Time for test: <em>{this.state.testTitle}</em></h3>
+            <form onSubmit={this.submitButton}>
+                <div className="form-group">
+                    <label htmlFor="startDate">Start Date:</label>
+                    <input id="startDate" value={this.state.startDate} onChange={this.startDateChange} className="form-control" type="date"></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="startTime">Start Time</label>
+                    <input id="startTime" value={this.state.startTime} onChange={this.startTimeChange} className="form-control" type="time" ></input>
+                </div>
+                <div className="text-center mb-2">
+                    <button className="btn btn-success" type="submit" onClick={this.submitButton}>Submit </button>
+                </div>
+            </form>
+        </div>
+    );
+    }
+}
+
+
+export default EditTestStartDateTime;
