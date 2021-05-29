794bc8b71304ea5beaa53b0d04f48c3cf509bb9a
apekovic <pekovic.andrijana@gmail.com>
2021-05-29 18:47:23 +0200

    Made visual and functional changes to Test Frontend component

--- testing-system-frontend/src/components/testlist/Test.js
+++ testing-system-frontend/src/components/testlist/Test.js
@@ -11,36 +11,41 @@ class Test extends React.Component {
             testExaminerId: this.props.testExaminerId,
             testStartDateTime: this.props.testStartDate,
             testStartDate: moment(this.props.testStartDate).format("Do MMM 'YY"),
-            testStartTime: moment(this.props.testStartDate).format("h:mm:ss a"),
+            testStartTime: moment(this.props.testStartDate).format("hh:mm:ss a"),
             testIsActive: this.props.testIsActive,
-            testScore: this.props.testScore
+            testScore: this.props.testScore,
+            value: this.props.testId,
+            onClick: null,
+            name: ""
         }
-    }
+    }    
 
     render() {
+        
         return (
             <div className="container">
-                <div className="row p-3">
+                <div className="row">
                     <div className="col-lg text-center">
                         { this.state.testTitle}
                     </div>
-                    <div className="col-sm text-center">
+                    <div className="col-lg text-center">
                         { this.state.testStartDate}
                     </div>
-                    <div className="col-sm text-center">
+                    <div className="col-lg text-center">
                         { this.state.testStartTime}
                     </div>
                     <div className="col-sm text-center">
                         { this.state.testScore}
                     </div>
-                    <div className="col-lg text-center">
-                        <button className="btn btn-success" onClick={this.props.activateOrDeactivateCallback} value={this.state.testId} name={Number(this.state.testIsActive)}> {(this.state.testIsActive ? "Deactivate" : "Activate")} </button>
-                    </div> 
-                    <div className="col-lg text-center">
-                        <button className="btn btn-success" onClick={this.props.questionCallback} value={this.state.testId} name={this.state.testTitle}>Questions</button>
-                    </div>                        
-                    <div className="col-lg text-center">
-                        <button className="btn btn-success" onClick={this.props.changeStartDateCallback} value={this.state.testId} name={this.state.testTitle}>Edit Date</button>
+                    <div className="col-sm text-center">
+                        <div className="dropdown">
+                            <button className="btn btn-primary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a className="dropdown-item" id={this.state.testId} name={Number(this.state.testIsActive)} onClick={this.props.activateOrDeactivateCallback}>{(this.state.testIsActive ? "Deactivate" : "Activate")}</a>
+                                <a className="dropdown-item" id={this.state.testId} name={this.state.testTitle} onClick={this.props.questionCallback} >Questions</a>
+                                <a className="dropdown-item" id={this.state.testId} name={this.state.testTitle} onClick={this.props.changeStartDateCallback}>Change Start Date</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
