00ebd29d5c178b5e91885ae525b01192d90c2948
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-06-11 13:42:24 +0200

    Core.Domain.Entities.Questions - addded questionType

---
+++ TestingSystem/Core/Domain/Entites/QuestionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Entites
+{
+    public enum QuestionType: byte
+    {
+        YesNo = 1,
+        FreeText = 2,
+        MultipleChoice = 3
+    }
+}
